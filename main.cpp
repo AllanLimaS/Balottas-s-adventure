eda8e64f170a2800cb41071d381ed558f28d9141
AllanLimaS <49033925+AllanLimaS@users.noreply.github.com>
2019-04-24 23:15:44 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,40 +1,71 @@
-#include "pirok.h"
+#include "entidade.h"
+#include "escada.h"
+#include "inimigo.h"
+#include "loja.h"
+#include "objeto.h"
+#include "personagem.h"
+#include "construcao_mapa.h"
+
 #include <QApplication>
 #include <QGraphicsScene>
 #include <QGraphicsRectItem>
 #include <QGraphicsView>
+#include <windows.h>
+#include <ctime>
 
 int main(int argc, char *argv[])
 {
+
+    int X = 0,Y = 0;
+
     QApplication a(argc, argv);
 
-        // create a scene
-        QGraphicsScene * scene = new QGraphicsScene();
-        scene->setSceneRect(0,0,1000,1000);
-        // create an item to add to the scene
-        for(int i =50 ; i < 1000 ; i=i +50){
-            scene->addLine(1,i,1000,i,QPen(5));
+    QGraphicsScene Tela_game;
+
+    Personagem * balottin = new Personagem();  
+    balottin->setRect(400,400,50,50);
+    balottin->setBrush(QBrush(Qt::blue));       // CRIA PRINCIPAL
+
+   Escada * saida = new Escada();
+
+    srand(time(NULL));       // RAND DA SAIDA
+    X = rand()%8;
+    Y = rand()%8;
+
+    X = (X * 50) + 50;      // POSIÇÃO X E Y
+    Y = (Y * 50) + 50;
+
+    saida->setRect(X, Y, 50, 50);       // CRIA SAIDA
+    saida->setBrush(QBrush(Qt::gray));
+
+    Tela_game.addItem(balottin);    // ADD PERSONAGEM
+    Tela_game.addItem(saida);       // ADD SAIDA
+
+
+
+    for(int i = 0 ; i <= 500 ; i=i +50){ // CRIA LINHAS
+        if(i == 0 or i==500){
+            Tela_game.addLine(1,i,500,i,QPen(Qt::black, 100, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+        }else {
+            Tela_game.addLine(1,i,500,i,QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
         }
-        for(int i = 50; i < 1000 ; i = i + 50){
-            scene->addLine(i,1,i,1000,QPen(2));
+    }
+    for(int i = 0; i <= 500 ; i = i + 50){ // CRIA LINHAS
+        if(i == 0 or i == 500){
+            Tela_game.addLine(i,1,i,500,QPen(Qt::black, 100, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+        }else{
+            Tela_game.addLine(i,1,i,500,QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
         }
-        scene->addLine(1,1,1000,1000,QPen(2));
-        pirok * rect = new pirok();
-        rect->setRect(300,300,50,50); // change the rect from 0x0 (default) to 100x100 pixels
-        rect->setBrush(QBrush(6));
-        // add the item to the scene
-        scene->addItem(rect);
+    }
 
-        // make rect focusable
-        rect->setFlag(QGraphicsItem::ItemIsFocusable);
-        rect->setFocus();
+    balottin->setFlag(QGraphicsItem::ItemIsFocusable); // MANTEM FOCO NO BALLOTIN
+    balottin->setFocus();
 
-        // create a view to visualize the scene
-        QGraphicsView * view = new QGraphicsView(scene);
 
-        // show the view
-        view->setFixedSize(1000,1000);
-        view->show();
+    QGraphicsView view(&Tela_game);
+    view.setFixedSize(700,700);
+    view.show();
 
-        return a.exec();
+    return a.exec();
+
 }
